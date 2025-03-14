c631d71df89efd4c6a9bd568c7cedb43bbd6ec7f
david emioma <davidemiomauche@gmail.com>
2025-03-14 11:56:41 +0000

    Final touches

--- app/boards/[boardId]/page.tsx
+++ app/boards/[boardId]/page.tsx
@@ -2,13 +2,15 @@ import { Room } from "@/components/room";
 import Canvas from "./_components/Canvas";
 import CanvasLoading from "./_components/CanvasLoading";
 
-export default function BoardPage({
-  params: { boardId },
+export default async function BoardPage({
+  params,
 }: {
-  params: {
+  params: Promise<{
     boardId?: string;
-  };
+  }>;
 }) {
+  const { boardId } = await params;
+
   return (
     <div className="w-full h-screen">
       <Room roomId={boardId as string} fallback={<CanvasLoading />}>
